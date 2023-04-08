35def044aaf6cb59f5930efd49980fca6b8415d7
Mansour Moufid <mansourmoufid@gmail.com>
2023-04-08 12:40:15 -0400

    entry: Add an on_changed argument to __init__() method.

--- ui/entry.py
+++ ui/entry.py
@@ -63,15 +63,29 @@ _entry_set_read_only.argtypes = [
 
 class EntryControl(control.Control):
 
-    def __init__(self, text=None, readonly=False, **kwargs):
+    def __init__(self, text=None, readonly=False, on_changed=None, **kwargs):
         super(EntryControl, self).__init__(**kwargs)
         self.ctrl = self.entry
-        self.callbacks = []
-        cb = _entry_on_changed.argtypes[1](self.on_changed)
-        _entry_on_changed(self.entry, cb, None)
-        self.callbacks += [cb]
         self.text(text)
         self.readonly(readonly)
+        self.callbacks = []
+        self.set_on_changed(on_changed or self.on_changed)
+
+    def on_changed(self):
+        pass
+
+    def set_on_changed(self, f=None):
+
+        if f is None:
+            return
+        self.on_changed = f
+
+        def _on_changed(entry, data):
+            return self.on_changed()
+
+        cb = _entry_on_changed.argtypes[1](_on_changed)
+        _entry_on_changed(self.entry, cb, None)
+        self.callbacks += [cb]
 
     def text(self, x=None):
         assert x is None or isinstance(x, str)
@@ -86,9 +100,6 @@ class EntryControl(control.Control):
             return not _entry_read_only(self.entry) == 0
         else:
             _entry_set_read_only(self.entry, 1 if x else 0)
-
-    def on_changed(self, entry, data):
-        pass
 
 
 # uiEntry *uiNewEntry(void);
