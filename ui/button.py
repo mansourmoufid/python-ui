417655ff966a1ba2a0ed0fa2a43c4c5535e7c5ef
Mansour Moufid <mansourmoufid@gmail.com>
2023-08-05 12:54:10 -0400

    button: Add typing information.

--- ui/button.py
+++ ui/button.py
@@ -27,6 +27,12 @@ _button_set_text.argtypes = [
 ]
 
 
+_button_on_clicked_callback_t = ctypes.CFUNCTYPE(
+    None,
+    ctypes.POINTER(_Button),
+    ctypes.c_void_p,
+)
+
 # void uiButtonOnClicked(
 #   uiButton *b,
 #   void (*f)(uiButton *b, void *data),
@@ -36,11 +42,7 @@ _button_on_clicked = libui.uiButtonOnClicked
 _button_on_clicked.restype = None
 _button_on_clicked.argtypes = [
     ctypes.POINTER(_Button),
-    ctypes.CFUNCTYPE(
-        None,
-        ctypes.POINTER(_Button),
-        ctypes.c_void_p,
-    ),
+    _button_on_clicked_callback_t,
     ctypes.c_void_p,
 ]
 
@@ -60,7 +62,7 @@ class Button(control.Control):
         text: str,
         on_clicked: typing.Optional[typing.Callable] = None,
         **kwargs
-    ):
+    ) -> None:
 
         super().__init__(**kwargs)
 
@@ -70,25 +72,36 @@ class Button(control.Control):
         self.callbacks: typing.List[typing.Callable] = []
         self.set_on_clicked(on_clicked or self.on_clicked)
 
-    def text(self, x=None):
-        assert x is None or isinstance(x, str)
+    def text(self, x: typing.Optional[str] = None) -> typing.Optional[str]:
         if x is None:
             return decode(_button_text(self.button))
         else:
             _button_set_text(self.button, encode(x))
+        return None
 
-    def on_clicked(self):
+    def on_clicked(self) -> None:
         pass
 
-    def set_on_clicked(self, f=None):
+    def set_on_clicked(
+        self,
+        f: typing.Optional[typing.Callable[[], None]] = None,
+    ) -> None:
 
         if f is None:
             return
         self.on_clicked = f
 
-        def _on_clicked(button, data):
+        def _on_clicked(
+            button: ctypes._Pointer,
+            data: ctypes.c_void_p,
+        ) -> None:
             return self.on_clicked()
 
-        cb = _button_on_clicked.argtypes[1](_on_clicked)
+        cb = _button_on_clicked_callback_t(_on_clicked)
         _button_on_clicked(self.button, cb, None)
         self.callbacks += [cb]
+
+    def __del__(self):
+        for cb in tuple(self.callbacks):
+            self.callbacks.remove(cb)
+            del cb
