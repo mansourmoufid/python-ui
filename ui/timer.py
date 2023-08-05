aa83b0db42ff7f6d8c05220ea8b25af8252a35dc
Mansour Moufid <mansourmoufid@gmail.com>
2023-08-05 12:37:12 -0400

    timer: Add typing information.

--- ui/timer.py
+++ ui/timer.py
@@ -1,34 +1,39 @@
 import ctypes
 import sys
 import traceback
+import typing
 
 from . import libui
 
+
+_timer_callback_t = ctypes.CFUNCTYPE(
+    ctypes.c_int,
+    ctypes.c_void_p,
+)
 
 # void uiTimer(int milliseconds, int (*f)(void *data), void *data);
 _timer = libui.uiTimer
 _timer.restype = None
 _timer.argtypes = [
     ctypes.c_int,
-    ctypes.CFUNCTYPE(
-        ctypes.c_int,
-        ctypes.c_void_p,
-    ),
+    _timer_callback_t,
     ctypes.c_void_p,
 ]
 
 
 class Timer(object):
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
-        self.callbacks = []
+        self.callbacks: typing.List[typing.Callable] = []
 
-    def after(self, t, f):
+    def after(
+        self,
+        t: typing.Union[int, float],
+        f: typing.Callable[[], typing.Union[bool, int]]
+    ) -> None:
 
-        assert isinstance(t, (int, float))
-
-        def _f(data):
+        def _f(data: ctypes.c_void_p) -> int:
             try:
                 if f():
                     return 1
@@ -39,10 +44,11 @@ class Timer(object):
                 traceback.print_exc(file=sys.stderr)
                 return 1
 
-        cb = _timer.argtypes[1](_f)
+        cb = _timer_callback_t(_f)
         _timer(int(float(t) * 1000), cb, None)
         self.callbacks += [cb]
 
     def __del__(self):
-        for cb in self.callbacks:
+        for cb in tuple(self.callbacks):
+            self.callbacks.remove(cb)
             del cb
